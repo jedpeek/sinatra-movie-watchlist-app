089f1228127809a33e6ec8fe6f008eedc1581ac8
Jed Peek <jedpeek@yahoo.com>
2018-08-08 20:03:21 +0000

    added movies controller routes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,42 @@
 class MoviesController < ApplicationController
 
   get '/movies' do
-    erb :'index'
+    @movies = Movie.all
+    erb :'/movies/index'
   end
+
+  get '/movies/new' do
+    erb :'/movies/new'
+  end
+
+  get '/movies/:slug' do
+    @movie = Movie.find_by_slug(params[:slug])
+    erb :'/movies/show'
+  end
+
+  post '/movies' do
+    @movie = Movie.create(name: params["Name"])
+    @movie.artist = User.find_or_create_by(name: params["User Name"])
+    @movie.genre_ids = params[:genres]
+    @movie.save
+
+    redirect to "/movies/#{@movie.slug}"
+  end
+
+  get '/movies/:slug/edit' do
+    @movie = Movie.find_by_slug(params[:slug])
+    erb :'movies/edit'
+  end
+
+  patch '/movies/:slug' do
+    @movie = Movie.find_by_slug(params[:slug])
+    @movie.update(params[:song])
+    @movie.artist = User.find_or_create_by(name: params["User Name"])
+    @movie.genre_ids = params[:genres]
+    @movie.save
+    redirect to "/movies/#{@movie.slug}"
+  end
+
 
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,6 @@ class UsersController < ApplicationController
 
     post '/signup' do
         @user = User.create(username: params[:username], email: params[:email], password: params[:password])
-        binding.pry
         session[:user_id] = @user.id
         redirect to '/movies'
     end
