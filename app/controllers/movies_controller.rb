b2693fc9a22cadf7debb21a3e4f5f32109b3e55e
Jed Peek <jedpeek@yahoo.com>
2018-08-08 21:56:10 +0000

    added new genre field

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,9 @@
 class MoviesController < ApplicationController
 
   get '/movies' do
-    if !logged_in?
+    if logged_in?
       @movies = Movie.all
+      binding.pry
       erb :'/movies/index'
     else
       redirect to '/login'
@@ -10,7 +11,7 @@ class MoviesController < ApplicationController
   end
 
   get '/movies/new' do
-    if !logged_in?
+    if logged_in?
       erb :'/movies/new'
     else
       redirect to '/login'
@@ -18,7 +19,7 @@ class MoviesController < ApplicationController
   end
 
   get '/movies/:slug' do
-    if !logged_in?
+    if logged_in?
       @movie = Movie.find_by_slug(params[:slug])
       erb :'/movies/show'
     else
@@ -36,7 +37,7 @@ class MoviesController < ApplicationController
   end
 
   get '/movies/:slug/edit' do
-    if !logged_in? && current_user == session[:id]
+    if logged_in? && current_user == session[:id]
       @movie = Movie.find_by_slug(params[:slug])
       erb :'movies/edit'
     else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     end
 
     get '/signup' do
-      if !!logged_in?
+      if !logged_in?
         erb :'/users/create_user'
       else
         redirect to '/movies'
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
 
 
     get '/login' do
-      if logged_in?
+      if !logged_in?
         erb :'/users/login'
       else
         redirect to '/movies'
@@ -30,7 +30,6 @@ class UsersController < ApplicationController
 
     post "/login" do
       user = User.find_by(:username => params[:username])
-
           if user && user.authenticate(params[:password])
               session[:user_id] = user.id
               redirect to "/movies"
